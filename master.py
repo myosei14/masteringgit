17ad25859f3196a782dce5b46a265811e92d1852
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2022-01-21 10:43:48 +0000

    Withdrawal disabled

--- master.py
+++ master.py
@@ -2,4 +2,4 @@ balance = 0
 deposit = int(input("How much do you want to deposit?"))
 print("You now have a balance of ", str(balance + deposit))
 print("Deposit more")
-print("Do you want to withdraw?")
+print("Withdrawal not available please try again later")
