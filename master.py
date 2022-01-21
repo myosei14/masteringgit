bf61f30cfb5868d3e58a1c4ad89b9be27f1961c9
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2022-01-21 10:37:09 +0000

    Withdrawal with current balance

--- master.py
+++ master.py
@@ -2,4 +2,9 @@ balance = 0
 deposit = int(input("How much do you want to deposit?"))
 print("You now have a balance of ", str(balance + deposit))
 print("Deposit more")
-print("Do you want to withdraw?")
+withdrawal = int(input("How much do you want to withdraw?"))
+if withdrawal > deposit:
+    print("Insufficient balance")
+
+else:
+    print("Your current balance is", str(deposit - withdrawal))
