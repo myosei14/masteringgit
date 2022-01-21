5c828795907aae3b257077de565675d65607923e
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2022-01-21 10:53:55 +0000

    Withdrawal unavailable updated

--- master.py
+++ master.py
@@ -1,5 +1,6 @@
 balance = 0
 deposit = int(input("How much do you want to deposit?"))
 print("You now have a balance of ", str(balance + deposit))
-print("Deposit more")
-print("Withdrawal not available please try again later")
+print("Withdrawal not available, try again later")
+
+
