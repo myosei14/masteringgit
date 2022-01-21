2d5eb6041fcae0c41eca5b0c682ef8b474706f20
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2022-01-21 10:55:28 +0000

    No withdrawal

--- master.py
+++ master.py
@@ -1,6 +1,5 @@
 balance = 0
 deposit = int(input("How much do you want to deposit?"))
 print("You now have a balance of ", str(balance + deposit))
-print("Withdrawal not available, try again later")
 
 
