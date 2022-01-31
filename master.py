4a74c1ca5a7f142b012abd4f7307d1901209ce73
Maxwell Osei Yeboah <34064325+myosei14@users.noreply.github.com>
2022-01-31 09:49:18 +0000

    Check balance

--- master.py
+++ master.py
@@ -4,6 +4,7 @@ print("You now have a balance of ", str(balance + deposit))
 withdrawal = int(input("How much do you want to withdraw?"))
 if withdrawal > deposit:
     print("Insufficient balance")
+    print("Please, Check your balance")
 
 else:
     print("Your current balance is", str(deposit - withdrawal))
